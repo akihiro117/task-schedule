6ec1b9a3425c7c94d0cf3e40fedacf7aa91d097b
Akihiro Yamada <u.parr917@gmail.com>
2018-12-08 17:24:13 +0900

    initialize spring boot application

---
+++ src/main/java/com/webapp/taskschedule/TaskScheduleApplication.java
@@ -0,0 +1,12 @@
+package com.webapp.taskschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskScheduleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/webapp/taskschedule/TaskScheduleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webapp.taskschedule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskScheduleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

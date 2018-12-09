1eb90cf0a93e66687401ef47401b859f9e2f3118
Akihiro Yamada <u.parr917@gmail.com>
2018-12-09 12:45:35 +0900

    add a member registration confirm form and a controller that move to member registration result page.

--- src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
+++ src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
@@ -1,13 +1,18 @@
 package com.webapp.taskschedule.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.webapp.taskschedule.form.MemberRegistrationForm;
 
 @Controller
+@SessionAttributes(value = "form")
 public class RegistMemberController {
+
+
 	@RequestMapping("/RegistForm")
 	String showRegistMemberForm() {
 
@@ -15,8 +20,20 @@ public class RegistMemberController {
 	}
 
 	@RequestMapping("/MemberRegistrationConfirm")
-	String showRegistrationConfirm(@ModelAttribute MemberRegistrationForm form) {
+	String showRegistrationConfirm(@ModelAttribute MemberRegistrationForm form,
+			Model model) {
+		//TODO:サーバーサイドの入力チェックを実装。
+
+		//確認画面への表示と登録をするため、値をsessionにセット。
+		model.addAttribute("form", form);
+
 		return "MemberRegistrationConfirm";
+	}
+
+	@RequestMapping("/MemberRegistrationResult")
+	String registMember() {
+
+		return "MemberRegistrationResult";
 	}
 
 }

999c99ea03e068127f43ad020c37d934ae49736a
Akihiro Yamada <u.parr917@gmail.com>
2018-12-09 11:52:25 +0900

    add a controller that move to member registration confirm form and form class.

--- src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
+++ src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
@@ -1,7 +1,10 @@
 package com.webapp.taskschedule.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.webapp.taskschedule.form.MemberRegistrationForm;
 
 @Controller
 public class RegistMemberController {
@@ -9,6 +12,11 @@ public class RegistMemberController {
 	String showRegistMemberForm() {
 
 		return "RegistForm";
+	}
+
+	@RequestMapping("/MemberRegistrationConfirm")
+	String showRegistrationConfirm(@ModelAttribute MemberRegistrationForm form) {
+		return "MemberRegistrationConfirm";
 	}
 
 }
---
+++ src/main/java/com/webapp/taskschedule/form/MemberRegistrationForm.java
@@ -0,0 +1,46 @@
+package com.webapp.taskschedule.form;
+
+
+public class MemberRegistrationForm {
+	private String name;
+
+	private String eMail;
+
+	private String password;
+
+	private String passwordConfirm;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+
+}

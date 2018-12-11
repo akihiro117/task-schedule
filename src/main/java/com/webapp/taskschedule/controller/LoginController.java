402cffb6943359dbac3f0a6baf4692fa4d607417
Akihiro Yamada <u.parr917@gmail.com>
2018-12-11 21:22:21 +0900

    add comments to codes.

--- src/main/java/com/webapp/taskschedule/controller/LoginController.java
+++ src/main/java/com/webapp/taskschedule/controller/LoginController.java
@@ -1,3 +1,7 @@
+//LoginController.java
+//Created by Akihiro Yamada on 2018/12/09.
+//Copyright (c) 2018. All Rights Reserved.
+
 package com.webapp.taskschedule.controller;
 
 import org.springframework.stereotype.Controller;
@@ -8,6 +12,7 @@ public class LoginController {
 	@RequestMapping("/")
 	String showLoginForm() {
 
+		//ログイン画面へ遷移。
 		return "LoginForm";
 	}
 
--- src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
+++ src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
@@ -1,3 +1,7 @@
+//RegistMemberController.java
+//Created by Akihiro Yamada on 2018/12/09.
+//Copyright (c) 2018. All Rights Reserved.
+
 package com.webapp.taskschedule.controller;
 
 import org.springframework.stereotype.Controller;
@@ -12,27 +16,44 @@ import com.webapp.taskschedule.form.MemberRegistrationForm;
 @SessionAttributes(value = "form")
 public class RegistMemberController {
 
-
+	/**
+	 * 会員情報入力画面を表示。
+	 * @return 会員情報入力画面。
+	 */
 	@RequestMapping("/RegistForm")
 	String showRegistMemberForm() {
 
+		//会員情報入力画面に遷移。
 		return "RegistForm";
 	}
 
+	/**
+	 * 会員情報登録の確認画面を表示。
+	 * 会員情報の入力チェックを行う。
+	 * @param form 入力された会員情報。
+	 * @param model sessionを渡すために使用するModelオブジェクト。
+	 * @return 会員情報登録確認画面。
+	 */
 	@RequestMapping("/MemberRegistrationConfirm")
 	String showRegistrationConfirm(@ModelAttribute MemberRegistrationForm form,
 			Model model) {
 		//TODO:サーバーサイドの入力チェックを実装。
 
-		//確認画面への表示と登録をするため、値をsessionにセット。
+		//確認画面への表示と登録を行うため、値をsessionにセット。
 		model.addAttribute("form", form);
 
+		//会員情報登録確認画面。
 		return "MemberRegistrationConfirm";
 	}
 
+	/**
+	 * 入力された会員情報をDBに登録。
+	 * @return 会員登録の結果画面。
+	 */
 	@RequestMapping("/MemberRegistrationResult")
 	String registMember() {
 
+		//会員登録の結果画面。
 		return "MemberRegistrationResult";
 	}
 

8065129ff2002847cd7acd5e57926b2410d8b2d1
Akihiro Yamada <u.parr917@gmail.com>
2018-12-24 17:53:05 +0900

    add spring security.

---
+++ src/main/java/com/webapp/taskschedule/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+//WebSecurityConfig.java
+//Created by Akihiro Yamada on 2018/12/24.
+//Copyright (c) 2018. All Rights Reserved.
+
+
+package com.webapp.taskschedule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//ログインページを指定。
+		//ログインページへのアクセスは全員許可する。
+		http.formLogin()
+				.loginPage("/")
+				.permitAll();
+		http.authorizeRequests()
+				.anyRequest().authenticated();
+	}
+
+}

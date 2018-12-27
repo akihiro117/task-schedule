3b3d3cf26c31e26b68712880ae46a8b584410782
Akihiro Yamada <u.parr917@gmail.com>
2018-12-28 00:41:24 +0900

    add a operation that checks a correlation validity of inputted passwords.

--- src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
+++ src/main/java/com/webapp/taskschedule/controller/RegistMemberController.java
@@ -22,14 +22,14 @@ import com.webapp.taskschedule.service.RegistMemberService;
 public class RegistMemberController {
 
 	@Autowired
-	RegistMemberService registMemberService;
+	private RegistMemberService registMemberService;
 
 	/**
 	 * 会員情報入力画面を表示。
 	 * @return 会員情報入力画面。
 	 */
 	@RequestMapping("/RegistForm")
-	String showRegistMemberForm(Model model) {
+	public String showRegistMemberForm(Model model) {
 
 		model.addAttribute("source", "login");
 
@@ -48,12 +48,20 @@ public class RegistMemberController {
 	 * @return 会員情報登録確認画面。
 	 */
 	@RequestMapping("/MemberRegistrationConfirm")
-	String showRegistrationConfirm(@Validated @ModelAttribute MemberRegistrationForm memberRegistrationForm,
+	public String showRegistrationConfirm(@Validated @ModelAttribute MemberRegistrationForm memberRegistrationForm,
 			BindingResult result, Model model) {
+
+		//相関項目エラーの有無。
+		boolean hasCorrelationErr =
+				!memberRegistrationForm.getPassword().equals(memberRegistrationForm.getPasswordConfirm());
+
+		if (hasCorrelationErr) {
+			model.addAttribute("errMsg", "異なるパスワードが入力されています。");
+			return "RegistForm";
+		}
 
 		if (result.hasErrors()) {
 			//入力エラーがある場合。
-
 			return "RegistForm";
 		}
 
@@ -69,7 +77,7 @@ public class RegistMemberController {
 	 * @return 会員登録の結果画面。
 	**/
 	@RequestMapping(value = "/MemberRegistrationResult", params = "regist")
-	String registMember(@ModelAttribute("form") MemberRegistrationForm form) {
+	public String registMember(@ModelAttribute("form") MemberRegistrationForm form) {
 
 		MemberRegistrationEntity entity = new MemberRegistrationEntity();
 		entity.setEMail(form.geteMail());
@@ -81,7 +89,7 @@ public class RegistMemberController {
 	}
 
 	@RequestMapping(value = "/MemberRegistrationResult", params = "revise")
-	String returnRegistForm(@ModelAttribute("form") MemberRegistrationForm memberRegistrationForm, Model model) {
+	public String returnRegistForm(@ModelAttribute("form") MemberRegistrationForm memberRegistrationForm, Model model) {
 		model.addAttribute("source", "revise");
 
 

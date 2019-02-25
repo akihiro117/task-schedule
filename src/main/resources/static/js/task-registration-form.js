5c0378ccdc4b264bdb12d17efec02fba9ed4487e
Akihiro Yamada <u.parr917@gmail.com>
2019-02-25 22:09:35 +0900

    fix a bug of the duplication of a form.

--- src/main/resources/static/js/task-registration-form.js
+++ src/main/resources/static/js/task-registration-form.js
@@ -2,22 +2,58 @@
  *
  */
 
-function addScheduleForm() {
-	//formを追加するボタンの要素を取得。
-	var buttonElem = document.getElementById("add-form");
-	buttonElem.addEventListener("click", function() {
-		var scheduleForm = document.getElementById("schedule-form");
+//登録フォームを出力する際の初期処理。
+function init() {
 
-		var lastElement = scheduleForm.lastElementChild;
-
-		var clone = lastElement.cloneNode(true);
-
-		clone.childNodes[1].value = "";	
-		clone.childNodes[5].value = "";
-		clone.childNodes[9].value = "";
-	
-		scheduleForm.append(clone);
-
-    });
-
+    //datepickerを作成。
+    $( function() {
+        $("#date1").datepicker({
+            dateFormat: "yy/mm/dd"
+        });
+      } );
+    
+      $( function() {
+            $("#date2").datepicker({
+                dateFormat: "yy/mm/dd"
+            });
+      } );
 }
+
+//ボタンクリック時にフォームを追加。
+function addScheduleForm() {
+    //datepickerを使うフォームのIDの連番。
+    //既に2まで使用しているため、初期値は2。
+    var idCount = 2;
+
+    $('#add-form').click(function() {
+        //idの連番をインクリメント。
+        idCount++;
+
+        //formを追加するボタンの要素を取得。
+        var scheduleForm = document.getElementById("schedule-form");
+
+        //最後の子要素を取得。
+        var lastElement = scheduleForm.lastElementChild;
+
+        var clone = lastElement.cloneNode(true);
+        
+        //各フォームの値をリセット。
+        clone.childNodes[1].value = "";	
+        clone.childNodes[5].value = "";
+        clone.childNodes[9].value = "";
+
+        var newId = "date" + idCount;
+        clone.childNodes[1].id = newId;
+
+        scheduleForm.append(clone);
+
+        //datepickerを再作成。
+        //これをやらないとdatepickerが有効にならない。
+        $('#' + newId).removeClass('hasDatepicker');
+        $('#' + newId).datepicker({
+            dateFormat: "yy/mm/dd"
+        });
+    });
+}
+
+
